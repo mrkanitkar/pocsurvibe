4e5ce952114bb330a870f18b7c679a418d8436f8
maheshwar <mrkanitkar@gmail.com>
2026-04-20 16:48:32 +0200

    fix(SP-4): swift-format cleanup

--- SurVibeTests/SettingsViewAppearanceTests.swift
+++ SurVibeTests/SettingsViewAppearanceTests.swift
@@ -6,13 +6,15 @@ import Testing
 @Suite("SettingsView Appearance")
 struct SettingsViewAppearanceTests {
 
-    @Test func settingsViewConstructs() {
+    @Test
+    func settingsViewConstructs() {
         let view = SettingsView()
         _ = view
         #expect(true)
     }
 
-    @Test func appearanceSettingsViewConstructs() {
+    @Test
+    func appearanceSettingsViewConstructs() {
         let view = AppearanceSettingsView()
         _ = view
         #expect(true)
